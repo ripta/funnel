09f02af16b69afa4fd5dc17f4d579348cb2a07e7
Ripta Pasay <ripta@users.noreply.github.com>
2016-09-10 21:02:33 -0700

    funnel.go: more funnel options and robust error handling

--- funnel.go
+++ funnel.go
@@ -9,24 +9,44 @@ import (
 	"sync"
 )
 
-type FunnelExecutor struct{}
-type FunnelOptions struct {
-	ConfigFilename string
-}
-
 type LogTransformerFunc func(src []byte, message []byte) ([]byte, error)
 
-func Exec(opts *FunnelOptions, command string, commandArgs []string) error {
+type FunnelExecutor struct{}
+type FunnelOptions struct {
+	Format                string
+	LogTimestamp          bool
+	LogTimestampFieldName string
+	RedirectStderr        []string
+	RedirectStdout        []string
+}
+
+func (o *FunnelOptions) BuildTransformer() (LogTransformerFunc, error) {
+	switch o.Format {
+	case "json":
+		return jsonLogTransformer, nil
+	case "raw":
+		return rawLogTransformer, nil
+	default:
+		return nil, fmt.Errorf("unrecognized format option %q", o.Format)
+	}
+}
+
+func Exec(opts *FunnelOptions, command string, commandArgs []string) (int, error) {
+	logTransformer, err := opts.BuildTransformer()
+	if err != nil {
+		return -1, err
+	}
+
 	cmd := exec.Command(command, commandArgs...)
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		return err
+		return -1, err
 	}
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return err
+		return -1, err
 	}
 
 	// fileMode := os.O_WRONLY | os.O_APPEND | os.O_CREATE
@@ -45,25 +65,25 @@ func Exec(opts *FunnelOptions, command string, commandArgs []string) error {
 
 	errFile := os.Stdout
 	outFile := os.Stdout
-	logTransformer := jsonLogTransformer
 
 	workers := sync.WaitGroup{}
 	dispatchBufferWorker(&workers, logTransformer, []byte("stderr"), errFile, stderr)
 	dispatchBufferWorker(&workers, logTransformer, []byte("stdout"), outFile, stdout)
 
 	if err = cmd.Start(); err != nil {
-		return err
+		return -1, err
 	}
 
 	// Workers must finish reading from the pipes before we can cmd.Wait()
 	workers.Wait()
 
-	// fmt.Fprintf(os.Stderr, "main done\n")
+	// The caller can optionally handle specific error codes themselves;
+	// see `cmd/funnel/action.go` for an example
 	if err = cmd.Wait(); err != nil {
-		return err
+		return -1, err
 	}
 
-	return nil
+	return 0, nil
 }
 
 func dispatchBufferWorker(workers *sync.WaitGroup, logTransformer LogTransformerFunc, src []byte, w io.Writer, r io.ReadCloser) {
